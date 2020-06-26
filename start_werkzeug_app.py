aad8a6c4ae6a62adeef2abf03bdf1624b7695ede
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-06-26 18:33:53 +0800

    测试serving

--- start_werkzeug_app.py
+++ start_werkzeug_app.py
@@ -1,5 +1,8 @@
 
 from werkzeug.read_codes.ReadSesseion import start_session_app
+from werkzeug.read_codes.ReadServing import start_server
 
 if __name__ == '__main__':
-    start_session_app()
+    #start_session_app()
+    #启动server,测试请求流程
+    start_server()
