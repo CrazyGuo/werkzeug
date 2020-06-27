a1967531539cd5fb0ca94dd090c5053981df43c1
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-06-27 10:22:41 +0800

    引入路由测试

--- start_werkzeug_app.py
+++ start_werkzeug_app.py
@@ -1,8 +1,13 @@
 
 from werkzeug.read_codes.ReadSesseion import start_session_app
 from werkzeug.read_codes.ReadServing import start_server
+from werkzeug.read_codes.ReadRouting import start_app_with_routing
 
 if __name__ == '__main__':
     #start_session_app()
+
     #启动server,测试请求流程
-    start_server()
+    #start_server()
+    
+    #测试routing
+    start_app_with_routing()
--- werkzeug/read_codes/__init__.py
+++ werkzeug/read_codes/__init__.py
@@ -1,2 +1,3 @@
 from . import ReadSesseion
 from . import ReadServing
+from . import ReadRouting
